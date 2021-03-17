34ac32c9184325b994e70a2775bf3d3189e3ab11
lacort <lacort2@gmail.com>
2021-03-17 10:55:52 -0300

    Update

--- app/components/scheduler.js
+++ app/components/scheduler.js
@@ -7,21 +7,29 @@ const objConfig = require('../../config/dev/main');
 
 
 async function run() {
-    
+
     const client = await MongoClient.connect(objConfig.db_agend.strConnection, objConfig.db_agend.options);
-    const agenda = new Agenda().mongo(client.db("nextAgend"), "jobs");
-    agenda.define('hello', () => {
-        console.log('hello, World!');
-        process.exit(0);
+    const agenda = new Agenda({
+        maxConcurrency: 5000,
+        processEvery: "30 seconds"
+    }).mongo(client.db("nextAgend"), "jobs");
+    agenda.define('hello', job => {
+        console.log('hello, World!',job.attrs.data.message);
+        // process.exit(0);
 
     });
-   
-
-
+    
     await new Promise(resolve => agenda.once('ready', resolve));
-
-    agenda.schedule(new Date(Date.now() + 2000), 'hello');
+    //agenda.cancel({ name: "hello" })
+    let i;
+    for(i=0;i<10000;i++){
+        agenda.schedule(new Date(Date.now() + 30000), 'hello',{
+            message: i
+        });
+        
+    }
     agenda.start();
+    
 }
 run().catch(error => {
     console.error(error);
