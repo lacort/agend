e713df22f7e7d5d045f2c16d08f0b0f0c3050787
Jefferson Lacort <lacort@pop-os.localdomain>
2021-03-17 18:49:31 -0300

    Update

--- app/components/scheduler.js
+++ app/components/scheduler.js
@@ -4,34 +4,50 @@ const {
 } = require('mongodb');
 const objConfig = require('../../config/dev/main');
 
-
-
-async function run() {
-
-    const client = await MongoClient.connect(objConfig.db_agend.strConnection, objConfig.db_agend.options);
-    const agenda = new Agenda({
-        maxConcurrency: 5000,
-        processEvery: "30 seconds"
-    }).mongo(client.db("nextAgend"), "jobs");
-    agenda.define('hello', job => {
-        console.log('hello, World!',job.attrs.data.message);
-        // process.exit(0);
-
-    });
-    
-    await new Promise(resolve => agenda.once('ready', resolve));
-    //agenda.cancel({ name: "hello" })
-    let i;
-    for(i=0;i<10000;i++){
-        agenda.schedule(new Date(Date.now() + 30000), 'hello',{
-            message: i
-        });
-        
+class AgendaTeste {
+    constructor() {
+        this.agenda;
     }
-    agenda.start();
-    
+
+    async start() {
+        const client = await MongoClient.connect(objConfig.db_agend.strConnection, objConfig.db_agend.options);
+        this.agenda = await new Agenda({
+            maxConcurrency: 20000,
+            processEvery: "10 seconds"
+        }).mongo(client.db("nextAgend"), "jobs");
+
+        this.agenda.define('nextAgenda', async job => {
+            console.log('hello, World!', job.attrs.data.message);
+            
+
+        });
+
+        await new Promise(resolve => this.agenda.once('ready', resolve));
+        this.agenda.start()
+    }
+    async schedule(intTime, strName, objData) {
+
+        this.agenda.schedule(new Date(Date.now() + intTime), strName, objData)
+        //this.agenda.start();
+
+    }
+
+    async update(strName, intTime,intRepeat) {
+        let job = await this.agenda.jobs({
+            name: strName
+        }, {
+            data: -1
+        })
+        
+        job[0].attrs.repeatInterval = intRepeat
+        job[0].attrs.lockedAt = null
+        job[0].attrs.nextRunAt = new Date(Date.now() + intTime);
+        
+        await job[0].save()
+        console.log(job[0].attrs)
+    }
+
 }
-run().catch(error => {
-    console.error(error);
-    process.exit(-1);
-});
+
+
+module.exports = new AgendaTeste();
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 require('dotenv').config();
-
+const sleep = require('system-sleep')
 const agendMongo = require('./app/components/mongodbAgend');
 // const agendModel = require('./app/models/AgendModel');
-const scheduler = require('./app/components/scheduler');
+const teste = require('./app/components/scheduler');
 const logger = require('./app/components/logger');
 
 
 
+teste.start()
+sleep(5000)
 
+//teste.schedule(60000,'nextAgenda',{message:'Gloria'})
 
-scheduler
+teste.update('nextAgenda',10000,'')
 
 logger.info('testando agendamento')
 
