f4136303a060b792b36079af4036bf6ecde1d442
Jefferson Lacort <lacort@pop-os.localdomain>
2021-03-18 12:16:36 -0300

    Update

--- app/components/scheduler.js
+++ app/components/scheduler.js
@@ -1,8 +1,10 @@
 const Agenda = require('agenda');
+const logger = require('./logger');
 const {
     MongoClient
 } = require('mongodb');
 const objConfig = require('../../config/dev/main');
+const fs = require('fs');
 
 class AgendaTeste {
     constructor() {
@@ -12,16 +14,28 @@ class AgendaTeste {
     async start() {
         const client = await MongoClient.connect(objConfig.db_agend.strConnection, objConfig.db_agend.options);
         this.agenda = await new Agenda({
-            maxConcurrency: 20000,
+            maxConcurrency: 2000,
             processEvery: "10 seconds",
-            defaultLockLifetime : 0
+            defaultLockLifetime: 0
         }).mongo(client.db("nextAgend"), "jobs");
 
         this.agenda.define('nextAgenda', async job => {
-            console.log('Send msg, id:', job.attrs.data.id);
+            let dateNow = new Date(Date.now())
+            let data = `Data Agora: ${dateNow}, Agendado: ${job.attrs.lastRunAt}, id: ${job.attrs.data.id} \n`
+            console.log(data)
+            // logger.error(data)
             
 
+
         });
+        // let date = new Date('2021-03-18T14:33:03.904Z');
+        // this.agenda.cancel({name:'nextAgenda'})
+        // let i;
+        // for (i = 0; i < 2000; i++) {
+        //     this.agenda.schedule(date, 'nextAgenda', {
+        //         id: i + 1
+        //     })
+        // }
 
         await new Promise(resolve => this.agenda.once('ready', resolve));
         this.agenda.start()
@@ -29,24 +43,24 @@ class AgendaTeste {
     async schedule(intTime, strName, objData) {
 
         this.agenda.schedule(new Date(Date.now() + intTime), strName, objData)
-       
+
 
     }
 
-    async update(strName, intTime,intRepeat) {
+    async update(strName, intTime, intRepeat) {
         let job = await this.agenda.jobs({
             name: strName
         }, {
             data: -1
         })
-        
+
         job[0].attrs.repeatInterval = intRepeat
         job[0].attrs.lockedAt = null
         job[0].attrs.nextRunAt = new Date(Date.now() + intTime);
-        
+
         await job[0].save()
         //console.log(job[0].attrs)
-        
+
     }
 
 }
